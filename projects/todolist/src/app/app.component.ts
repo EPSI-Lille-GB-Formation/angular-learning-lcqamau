b44792969b600e126255711e37e40e60edff5002
lcqamau <104760365+lcqamau@users.noreply.github.com>
2023-11-17 13:47:51 +0100

    Premier commit

---
+++ projects/todolist/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: '<h1>Hello Word</h1>',
+  styles: []
+})
+export class AppComponent {
+  
+}
