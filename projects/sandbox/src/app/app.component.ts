7ed65653a5675c574bf9ec89d01c4d8327af6315
lcqamau <104760365+lcqamau@users.noreply.github.com>
2023-12-08 08:56:54 +0100

    commit 2

--- projects/sandbox/src/app/app.component.ts
+++ projects/sandbox/src/app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HighlightDirective } from './highlight.directive';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: '<h1>Hello word !</h1>',
-  styles: []
+  imports: [CommonModule, RouterOutlet,HighlightDirective],
+  template: `
+    <h1>Découverte des directives avec Angular</h1>
+ 
+    <a>Go to Google</a>
+ 
+    <p appHighlight >
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque sint,
+      recusandae aliquam illo aliquid doloribus neque natus magnam et! Eius
+      asperiores ullam amet explicabo veniam aspernatur sint id sunt autem?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque sint,
+      recusandae aliquam illo aliquid doloribus neque natus magnam et! Eius
+      asperiores ullam amet explicabo veniam aspernatur sint id sunt autem?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque sint,
+      recusandae aliquam illo aliquid doloribus neque natus magnam et! Eius
+      asperiores ullam amet explicabo veniam aspernatur sint id sunt autem?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque sint,
+      recusandae aliquam illo aliquid doloribus neque natus magnam et! Eius
+      asperiores ullam amet explicabo veniam aspernatur sint id sunt autem?
+    </p>
+  `,
+  styles: [],
 })
-export class AppComponent {
-  title = 'sandbox';
-}
+export class AppComponent {}
--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TODOS } from './mock-todo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: '<h1>Hello Word</h1>',
+  template: `
+  <h1>Listes des choses à faire</h1>
+    <ul *ngFor="let todo of todolist">
+      <li *ngIf="todo.isCompleted">
+        {{ todo.title }}
+      </li>
+    </ul>
+  `,
   styles: []
 })
 export class AppComponent {
-  
+  todolist = TODOS;
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    console.log(this.todolist)
+  };
+
+  selectTodo(id:number){
+    console.table(this.todolist[id-1])
+  }
 }
